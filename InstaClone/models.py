773a78d4224d124edd0d1dbad29d00455c5ddfc3
Ian K <iankoech9@gmail.com>
2021-01-16 16:31:49 +0300

    Installed psycopg2

--- InstaClone/models.py
+++ InstaClone/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    '''
+    Class creates columns in the image table
+    '''
+    name = models.CharField(max_length = 30)
+    caption = models.CharField(max_length = 30)
+    
+    
