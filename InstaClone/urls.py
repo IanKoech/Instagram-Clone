d1297c736e794fa2e94e42dc6bce2821e9a57c66
Ian K <iankoech9@gmail.com>
2021-01-18 19:38:41 +0300

    Completed urlconf

--- InstaClone/urls.py
+++ InstaClone/urls.py
@@ -4,8 +4,13 @@ from . import views
 from django.conf.urls  import url
 
 urlpatterns = [
-    url(r'^$',views.photos, name = photos),
-    url()
+    url(r'^$',views.photos, name = 'photos'),
+    url(r'^profile/(\d+)$', views.profile, name = 'profile'),
+    url(r'^new/$', views.addImages, name = 'addImages'),
+    url(r'^update/$', views.updateProfile, name = 'profileUpdate'),
+    url(r'^comment/(\d+)$', views.new_comment, name = 'comment'),
+    url(r'^like/(\d+)$', views.like, name = 'like'),
+    url(r'search/$', views.search, name = 'search')
 ]
 
 if settings.DEBUG:
