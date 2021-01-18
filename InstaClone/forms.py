407ae5fd79ca8ac6e0ab1868f64010c255089cc4
Ian K <iankoech9@gmail.com>
2021-01-18 15:34:22 +0300

    Updated forms

--- InstaClone/forms.py
+++ InstaClone/forms.py
@@ -1,5 +1,17 @@
 from django import forms
+from .models import Image, Comments, Profile
 
 class NewImageForm(forms.ModelForm):
     class Meta:
-        
+        model = Image
+        exclude = ['user', 'likes', 'captions']
+
+class UpdateProfile(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude =  ['user']
+
+class Comment(forms.ModelForm):
+    class Meta:
+        model = Comments
+        exclude = ['user', 'photo']
